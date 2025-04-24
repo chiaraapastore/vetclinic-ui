2975c872b5d6ec945306c31c01941a27510b78da
chiarapastore <chiarapastoree001@gmail.com>
2025-04-25 01:29:06 +0200

    Initial commit

---
+++ src/app/profilo/profilo.component.ts
@@ -0,0 +1,100 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {UtenteService} from '../services/utente.service';
+import {HttpClient} from '@angular/common/http';
+import {ToastrService} from 'ngx-toastr';
+import {Location} from '@angular/common';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-profilo',
+  standalone: false,
+  templateUrl: './profilo.component.html',
+  styleUrls: ['./profilo.component.css']
+})
+export class ProfiloComponent implements OnInit {
+  userDetails: any = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    registrationNumber: '',
+    reparto: { name: '' },
+    profileImage: 'https://i.pinimg.com/736x/57/38/48/573848ebfeef7475d442c045a5ba9643.jpg',
+    nameDepartment: ''
+  };
+  selectedFile: File | null = null;
+
+  @ViewChild('fileInput') fileInput!: ElementRef;
+
+  constructor(private utenteService: UtenteService,private router: Router ,private http: HttpClient, private toastr: ToastrService, private location: Location) {}
+
+  ngOnInit(): void {
+
+    this.utenteService.getUserInfo().subscribe(
+      (data) => {
+        if (data) {
+          this.userDetails = data;
+          if (!this.userDetails.reparto) {
+            this.userDetails.reparto = { name: 'Nessun reparto assegnato' };
+          }
+          if (!this.userDetails.profileImage) {
+            this.userDetails.profileImage = 'https://i.pinimg.com/736x/57/38/48/573848ebfeef7475d442c045a5ba9643.jpg';
+          }
+        }
+        console.log('Dati utente ricevuti:', data);
+
+      },
+
+      (error) => {
+        console.error('Errore nel recupero dati utente', error);
+      }
+    );
+  }
+
+  triggerFileInput(): void {
+    this.fileInput.nativeElement.click();
+  }
+
+  goBack(): void {
+    this.router.navigate(['/assistente']);
+  }
+
+  onFileSelected(event: any): void {
+    if (event.target.files.length > 0) {
+      this.selectedFile = event.target.files[0];
+
+      if (this.selectedFile) {
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.userDetails.profileImage = e.target.result;
+        };
+        reader.readAsDataURL(this.selectedFile as Blob);
+      }
+    }
+  }
+
+
+  updateProfile(): void {
+    if (this.selectedFile) {
+      const formData = new FormData();
+      formData.append('file', this.selectedFile);
+
+      this.http.post(`/api/upload-profile-image/${this.userDetails.id}`, formData).subscribe(
+        (response: any) => {
+          console.log('Immagine caricata', response);
+          this.userDetails.profileImage = response.imageUrl;
+        },
+        (error) => console.error('Errore nel caricamento dell\'immagine', error)
+      );
+    }
+
+    this.utenteService.updateUtente(this.userDetails.id, this.userDetails).subscribe(
+      () => {
+        this.toastr.success('Profilo aggiornato con successo!', 'Successo');
+      },
+      (error) => {
+        console.error('Errore aggiornamento profilo', error);
+        this.toastr.error('Errore aggiornamento profilo', 'Errore');
+      }
+    );
+  }
+}
